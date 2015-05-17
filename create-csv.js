51e26cdc064ec8ee27fe121e11bf19c6ba93ab10
Miirka <karim.development@hotmail.com>
2015-05-17 19:05:41 +0100

    commit 4

--- create-csv.js
+++ create-csv.js
@@ -60,6 +60,6 @@ $('tbody tr', $table).each(function(){
 
 var data = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
 
-$('body').append('<a href="'+data+'" download="statement-'+(statement_date.replace(' ', '-'))+'.csv" id="download-statement" style="display: none;">Download</a>');
+$('body').append('<a href="'+data+'" download="statement-'+(statement_date.replace(/ /g, "-"))+'.csv" id="download-statement" style="display: none;">Download</a>');
 
 $('#download-statement')[0].click();
